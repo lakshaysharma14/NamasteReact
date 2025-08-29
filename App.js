2be5b361e4ddc1032780bb25a34a609f87d13da7
lakshaysharma14 <laksh1013shar@gmail.com>
2025-08-29 12:39:34 +0530

    Starting Swiggy

--- App.js
+++ App.js
@@ -1,19 +1,3 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = (
-  <h1 id="title" key="h1">
-    Hello World from React using JSX 🚀
-  </h1>
-);
-
-const HeadingComponent = () => {
-  return (
-    <h1 id="title" key="h1">
-      Hello World from React using Functional Component  🚀
-    </h1>
-  );
-};
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeadingComponent />);
